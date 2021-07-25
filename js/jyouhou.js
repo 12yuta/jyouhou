189407da81232cdc23e72878c2f523f3287f3af9
12yuta <87630733+12yuta@users.noreply.github.com>
2021-07-25 11:22:11 +0900

    Add files via upload

--- js/jyouhou.js
+++ js/jyouhou.js
@@ -1,7 +1,10 @@
 
 let count = 0;
 const mainElement = document.querySelector("div.main>img");
-const url = [  "https://www.football-zone.net/wp-content/uploads/2020/08/20200829_Leonardo-Bonucci_ap.jpg" ,
+
+const url = [
+        "https://images.beinsports.com/VicveZeoG-dXOyww1dSMR05zbDo=/full-fit-in/1000x0/unaisimon-cropped_wtaiiv29slb2195zl445lpasg.jpg",
+        "https://www.football-zone.net/wp-content/uploads/2020/08/20200829_Leonardo-Bonucci_ap.jpg" ,
         "https://www.realmadrid.com/cs/Satellite?blobcol=urldata&blobheader=image%2Fjpeg&blobkey=id&blobtable=MungoBlobs&blobwhere=1203415743550&ssbinary=true",
         "https://www.football-zone.net/wp-content/uploads/2020/08/20200829_Leonardo-Bonucci_ap.jpg",
         "https://amd-pctr.c.yimg.jp/r/iwiz-amd/20210708-00010021-footballc-000-1-view.jpg",
